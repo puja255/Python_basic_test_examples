01edecb1311538ad2702c829bbc749e0543709e6
puja_aina <puja.aina@gmail.com>
2023-07-27 05:09:18 +0530

    first_commit

---
+++ add_two_numbers.py
@@ -0,0 +1,8 @@
+# Program to add two numbers
+
+n1 = input("Enter first number")
+print(type(n1))
+n2 = input("Enter second number")
+print(type(n2))
+sum_n1_n2 = int(n1) + int(n2)
+print(sum_n1_n2)
